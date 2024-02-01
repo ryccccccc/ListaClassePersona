c614f04f9071f6d62529e640fdac0ef63a0e62c2
Riccardo Nervosi <riccardo.nervosi@gmail.com>
2024-02-01 18:35:33 +0100

    Correzione stampa

---
+++ ListaClassePersona/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaClassePersona
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public int Età { get; set; }
+        public Persona(string name,int age) 
+        {
+            Nome = name;
+            Età = age;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1}", Nome, Età);
+        }
+
+        public static Persona Parse(string person)
+        {
+            string[] props = person.Split(',');
+            Persona ptemp = new Persona((props[0]), int.Parse(props[1]));
+            return ptemp;
+        }
+    }
+}
---
+++ ListaClassePersona/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaClassePersona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> persone = new List<Persona>();
+            int c = 0;
+            while (c<6)
+            {
+                persone.Add(Persona.Parse(Console.ReadLine()));
+                c++;
+            }
+
+            Console.WriteLine("I Maggiorenni sono:");
+            foreach (Persona pers in persone)
+            {
+                if(pers.Età >= 18)
+                {
+                    Console.WriteLine(pers);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
